257ac9034e09fc96896a07addcea16c41c32c107
jita1218 <131987625+jita1218@users.noreply.github.com>
2024-07-02 01:37:30 +0530

    added frontend url

--- backend/server.js
+++ backend/server.js
@@ -1,46 +1,49 @@
 import path from "path";
 import express from "express";
-import  dotenv from "dotenv";
+import dotenv from "dotenv";
 import authRoutes from './routes/authRoutes.js';
-import connectToMongoDB from "./db/connectToMongoDB.js";
+import connectToMongoDB from "./db/connectToMongoDB.js"
 import messageRoutes from "./routes/messageRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import {app,server} from './socket/socket.js'
+import { app, server } from './socket/socket.js';
 import callRoutes from "./routes/callRoutes.js";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 8000;
-
 const __dirname = path.resolve();
 
+// Use FRONTEND_URL for CORS
+const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:3000';
 
+const corsOptions = {
+    origin: FRONTEND_URL,
+    credentials: true,
+    optionsSuccessStatus: 200 // Some legacy browsers choke on 204
+};
 
-
-app.use(cors({ origin: process.env.PORT, credentials: true }));
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
-app.use("/api/call",callRoutes);
+app.use("/api/call", callRoutes);
 
 app.use(express.static(path.join(__dirname, "/frontend/build")));
 
 app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
 });
 
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Something broke!');
-  });
+});
 
- 
-  
-      server.listen(PORT, () => {
-        connectToMongoDB();
-          console.log(`Server is running on port ${PORT}`);
-      });
+server.listen(PORT, () => {
+    connectToMongoDB();
+    console.log(`Server is running on port ${PORT}`);
+});
